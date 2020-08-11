bad217fcec92fdb3956635c843f5ea68fc88e96b
Tiago <tiago.asf@outlook.com>
2020-08-10 22:09:34 -0300

    CRUD implementado para a classe CategoryController

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -1,13 +1,46 @@
 using Microsoft.AspNetCore.Mvc;
+using Shop.Models;
 
 //https://localhost:5001/categories
 [Route("categories")]
 public class CategoryController : ControllerBase
 {
-    //https://localhost:5001/categories/olamundo
-    [Route("olamundo")]
-    public string MeuMetodo()
+    [HttpGet]
+    [Route("")]
+    public string Get()
     {
-        return "Olá mundo MVC";
+        return "GET";
     }
-}
+
+    [HttpGet]
+    [Route("{id:int}")]
+    public string GetById(int id)
+    {
+        return $"GET item #{id}";
+    }
+
+    [HttpPost]
+    [Route("")]
+    public Category Post([FromBody]Category model)
+    {
+        return model;
+    }
+
+    [HttpPut]
+    [Route("{id:int}")]
+    public Category Put(int id, [FromBody]Category model)
+    {
+        if (model.Id == id)
+        {
+            return model;
+        } 
+        return null;
+    }
+
+    [HttpDelete]
+    [Route("")]
+    public string Delete()
+    {
+        return "DELETE";
+    }
+} 
