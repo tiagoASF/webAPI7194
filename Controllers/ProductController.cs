b1228a7b2d97baba0de6475ac5feaa2622cbbb49
Tiago <tiago.asf@outlook.com>
2020-08-13 05:57:53 -0300

    GETs em ProductController implementados

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System;
 using Shop.Models;
 using Shop.Data;
+using System.Linq;
 
 namespace Shop.Controllers
 {
@@ -24,6 +25,41 @@ namespace Shop.Controllers
             return Ok(products);    
         }
 
-        
+        [HttpGet]
+        [Route("{id:int}")]
+        public async Task<ActionResult<Product>> GetById(
+            int id,
+            [FromServices]DataContext context
+        )
+        {
+            var product = await context
+                .Products
+                .Include(x => x.Category)
+                .AsNoTracking()
+                .FirstOrDefaultAsync(x => x.Id == id);
+            
+            return Ok(product);
+        }
+
+        //Método GET que pega todos os produtos de uma determinada categoria
+        //Https://products/categories/1
+        [HttpGet]
+        [Route("categories/{id:int}")]
+
+        public async Task<ActionResult<List<Product>>> GetByCategory([FromServices] DataContext context)
+        {
+            var products = await context
+                .Products
+                .Include(x => x.Category)
+                .AsNoTracking()
+                .Where(x => x.Category.Id == id)
+                .ToListAsync();
+            
+            return Ok(products);
+        }
+
+
+
+
     }
 }
