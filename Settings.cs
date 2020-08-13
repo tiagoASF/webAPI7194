52fdcdbd78709f093e11a9f8dadd121ed54b83f2
Tiago <tiago.asf@outlook.com>
2020-08-13 19:30:15 -0300

    criado o servico de autenticacao

---
+++ Settings.cs
@@ -0,0 +1,8 @@
+namespace Shop
+{
+    public static class Settings
+    {
+        //É uma boa prática gerar um GUID para servir de chave
+        public static string Secret = "345hgdfweropkgter8945nmfdk";
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,9 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Shop.Data;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace Shop
 {
@@ -21,6 +24,26 @@ namespace Shop
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            //Transforma a chave criada em shop.Settings para o formato de bytes
+            var key = Encoding.ASCII.GetBytes(Settings.Secret);
+            services.AddAuthentication( x => 
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false
+                    };
+            });
+
             //services.AddDbContext<DataContext>(opt => opt.UseInMemoryDatabase("Database"));
             services.AddDbContext<DataContext>(opt => opt.UseSqlServer(Configuration.GetConnectionString("connectionString")));
             services.AddScoped<DataContext, DataContext>();
@@ -37,6 +60,8 @@ namespace Shop
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseAuthorization();
 
