ef21f3c7c9257ef3defc59f6c09d2f46e7e9562e
Tiago <tiago.asf@outlook.com>
2020-08-13 06:39:40 -0300

    ProductController implementado

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -46,7 +46,10 @@ namespace Shop.Controllers
         [HttpGet]
         [Route("categories/{id:int}")]
 
-        public async Task<ActionResult<List<Product>>> GetByCategory([FromServices] DataContext context)
+        public async Task<ActionResult<List<Product>>> GetByCategory(
+            [FromServices] DataContext context,
+            int id 
+        )
         {
             var products = await context
                 .Products
@@ -58,7 +61,96 @@ namespace Shop.Controllers
             return Ok(products);
         }
 
+        [HttpPost]
+        [Route("")]
+        public async Task<ActionResult<List<Product>>> Post(
+            [FromBody]Product model,
+            [FromServices]DataContext context
+        )
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+                
+                context.Products.Add(model);
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new {message = "Não foi possível criar o produto"});
+            }
+            
+        }
 
+        [HttpPut]
+        [Route("{id:int}")]
+        public async Task<ActionResult<List<Product>>> Put(
+            int id,
+            [FromBody]Product model,
+            [FromServices]DataContext context
+        )
+        {
+            //Verifica se o ID é válido
+            if (model.Id != id)
+            {
+                return NotFound(new { message = "Produto não localizado"});
+            }
+
+            //Verifica se os dados são validos
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState); 
+            } 
+            
+            try
+            {
+                context.Entry<Product>(model).State = EntityState.Modified;
+                await context.SaveChangesAsync();
+                return Ok(model);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return BadRequest(new { message = "Esse registro já foi atualizado"});
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não foi possível atualizar o produto"});
+            }
+        }
+
+
+        [HttpDelete]
+        [Route("{id:int}")]
+        public async Task<ActionResult<List<Product>>> Delete(
+            int id,
+            [FromServices]DataContext context
+        )
+        //Para efetuar o DELETE, primeiro é necessário recuperar a categoria do banco
+        {
+            var product = await context.Products.FirstOrDefaultAsync(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound(new { message = "Produto não encontrado"});
+            }
+
+            try
+            {
+                context.Products.Remove(product);
+                await context.SaveChangesAsync();
+                return Ok(new { message = "Produto removido com sucesso"});
+                // ou
+                //return Ok(category);
+            }
+            catch (Exception)
+            {
+                return BadRequest(new { message = "Não é possível remover o produto"});
+            
+            }
+        }
 
 
     }
